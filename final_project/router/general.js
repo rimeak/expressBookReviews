5ef9a969a386938b5103d5872bddd3f6367a7a25
rimeak <akallalrime@gmail.com>
2023-02-19 17:00:09 -0500

    made changes on files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -81,13 +81,12 @@ public_users.get('/review/:isbn',function (req, res) {
     res.send(books[ISBN].reviews)
 });
 
+// Task 10
 function getBookList(){
   return new Promise((resolve,reject)=>{
     resolve(books);
   })
 }
-
-// Get the book list available in the shop
 public_users.get('/',function (req, res) {
   getBookList().then(
     (bk)=>res.send(JSON.stringify(bk, null, 4)),
@@ -96,20 +95,16 @@ public_users.get('/',function (req, res) {
 });
 
 // Task 11
-// Add the code for getting the book details based on ISBN (done in Task 2) using Promise callbacks or async-await with Axios.
-
 function getFromISBN(isbn){
   let book_ = books[isbn];  
   return new Promise((resolve,reject)=>{
     if (book_) {
       resolve(book_);
     }else{
-      reject("Unable to find book!");
+      reject("Unable to find book");
     }    
   })
 }
-
-// Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   getFromISBN(isbn).then(
@@ -119,8 +114,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
 
 // Task 12
-// Add the code for getting the book details based on Author (done in Task 3) using Promise callbacks or async-await with Axios.
-
 function getFromAuthor(author){
   let output = [];
   return new Promise((resolve,reject)=>{
@@ -133,8 +126,6 @@ function getFromAuthor(author){
     resolve(output);  
   })
 }
-
-// Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
   getFromAuthor(author)
@@ -144,9 +135,6 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Task 13
-// Add the code for getting the book details based on Title (done in Task 4) using Promise callbacks or async-await with Axios.
-
-
 function getFromTitle(title){
   let output = [];
   return new Promise((resolve,reject)=>{
@@ -159,8 +147,6 @@ function getFromTitle(title){
     resolve(output);  
   })
 }
-
-// Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
   getFromTitle(title)
@@ -168,81 +154,5 @@ public_users.get('/title/:title',function (req, res) {
     result =>res.send(JSON.stringify(result, null, 4))
   );
 });
-
-// Task 10 
-
-function getBookList(){
-    return new Promise((resolve,reject)=>{
-      resolve(books);
-    })
-  }
-  public_users.get('/',function (req, res) {
-    getBookList().then(
-      (bk)=>res.send(JSON.stringify(bk, null, 4)),
-      (error) => res.send("denied")
-    );  
-  });
-  
-  // Task 11
-  function getFromISBN(isbn){
-    let book_ = books[isbn];  
-    return new Promise((resolve,reject)=>{
-      if (book_) {
-        resolve(book_);
-      }else{
-        reject("Can't find book");
-      }    
-    })
-  }
-  
-  public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    getFromISBN(isbn).then(
-      (bk)=>res.send(JSON.stringify(bk, null, 4)),
-      (error) => res.send(error)
-    )
-   });
-  
-  // Task 12
-  function getFromAuthor(author){
-    let output = [];
-    return new Promise((resolve,reject)=>{
-      for (var isbn in books) {
-        let book_ = books[isbn];
-        if (book_.author === author){
-          output.push(book_);
-        }
-      }
-      resolve(output);  
-    })
-  }
-  public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    getFromAuthor(author)
-    .then(
-      result =>res.send(JSON.stringify(result, null, 4))
-    );
-  });
-  
-  // Task 13
-  function getFromTitle(title){
-    let output = [];
-    return new Promise((resolve,reject)=>{
-      for (var isbn in books) {
-        let book_ = books[isbn];
-        if (book_.title === title){
-          output.push(book_);
-        }
-      }
-      resolve(output);  
-    })
-  }
-  public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    getFromTitle(title)
-    .then(
-      result =>res.send(JSON.stringify(result, null, 4))
-    );
-  });
   
 module.exports.general = public_users;
